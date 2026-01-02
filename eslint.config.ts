8bbaa5ff7ab1bb44cbb75702ff184a03d3de70e1
cylewaitforit <cyle@cylewaitfor.it>
2026-01-01 21:16:25 -0600

    chore(dependabot): add dependabot config file (#49)
    * chore(dependabot): add dependabot config file
    
    * fix: knip

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
 import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
 import eslint from "@eslint/js";
 import vitest from "@vitest/eslint-plugin";
+import dependabot from "eslint-plugin-dependabot";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
 import jsdoc from "eslint-plugin-jsdoc";
 import jsonc from "eslint-plugin-jsonc";
@@ -11,12 +12,13 @@ import perfectionist from "eslint-plugin-perfectionist";
 import { configs } from "eslint-plugin-pnpm";
 import * as regexp from "eslint-plugin-regexp";
 import yml from "eslint-plugin-yml";
-import { defineConfig } from "eslint/config";
+import { yaml } from "eslint-yaml";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-	{
-		ignores: [
+	globalIgnores(
+		[
 			"**/*.snap",
 			"**/_fixtures_/",
 			"coverage",
@@ -24,34 +26,34 @@ export default defineConfig([
 			"node_modules",
 			"pnpm-lock.yaml",
 		],
-	},
-	{ linterOptions: { reportUnusedDisableDirectives: "error" } },
-	eslintPlugin.configs.recommended,
+		"Global Ignores",
+	),
 	{
-		files: ["src/rules/**/*.ts"],
-		name: "eslint-plugin rules",
-		rules: {
-			"eslint-plugin/require-meta-docs-description": "error",
-			"eslint-plugin/require-meta-docs-url": "error",
-			"eslint-plugin/require-meta-schema": "error",
-		},
+		linterOptions: { reportUnusedDisableDirectives: "error" },
+		name: "Global Linter Options",
 	},
-	eslint.configs.recommended,
-	comments.recommended,
-	jsdoc.configs["flat/contents-typescript-error"],
-	jsdoc.configs["flat/logical-typescript-error"],
-	jsdoc.configs["flat/stylistic-typescript-error"],
-	jsonc.configs["flat/recommended-with-json"],
-	markdown.configs.recommended,
-	n.configs["flat/recommended"],
-	packageJson.configs.recommended,
-	perfectionist.configs["recommended-natural"],
-	regexp.configs["flat/recommended"],
-	...configs.json,
-	...configs.yaml,
 	{
+		extends: [
+			comments.recommended,
+			eslint.configs.recommended,
+			eslintPlugin.configs.recommended,
+			jsdoc.configs["flat/contents-typescript-error"],
+			jsdoc.configs["flat/logical-typescript-error"],
+			jsdoc.configs["flat/stylistic-typescript-error"],
+			n.configs["flat/recommended"],
+			perfectionist.configs["recommended-natural"],
+			regexp.configs["flat/recommended"],
+			tseslint.configs.strictTypeChecked,
+			tseslint.configs.stylisticTypeChecked,
+		],
 		files: ["**/*.{js,ts}"],
-		name: "tsdoc tags",
+		languageOptions: {
+			parserOptions: {
+				projectService: { allowDefaultProject: ["*.config.*s"] },
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		name: "JavaScript and TypeScript files",
 		rules: {
 			// See: https://tsdoc.org/ and https://github.com/microsoft/tsdoc/blob/main/tsdoc/src/details/StandardTags.ts
 			"jsdoc/check-tag-names": [
@@ -74,22 +76,7 @@ export default defineConfig([
 					],
 				},
 			],
-		},
-	},
-	{
-		extends: [
-			tseslint.configs.strictTypeChecked,
-			tseslint.configs.stylisticTypeChecked,
-		],
-		files: ["**/*.{js,ts}"],
-		ignores: ["**/*.md/**"],
-		languageOptions: {
-			parserOptions: {
-				projectService: { allowDefaultProject: ["*.config.*s"] },
-				tsconfigRootDir: import.meta.dirname,
-			},
-		},
-		rules: {
+
 			// Stylistic concerns that don't interfere with Prettier
 			"logical-assignment-operators": [
 				"error",
@@ -106,21 +93,43 @@ export default defineConfig([
 		},
 	},
 	{
+		extends: [jsonc.configs["flat/recommended-with-json"]],
+		files: ["**/*.json"],
+		name: "JSON files",
+	},
+	{
+		extends: [markdown.configs.recommended],
+		files: ["**/*.md"],
+		name: "Markdown files",
+	},
+	{
 		extends: [tseslint.configs.disableTypeChecked],
 		files: ["**/*.md/*.{js,ts}"],
+		name: "Markdown code blocks",
 		rules: {
 			"n/no-missing-import": "off",
 			"n/no-unpublished-import": "off",
 		},
 	},
 	{
+		files: ["src/rules/**/*.ts"],
+		name: "ESLint Plugin Rule Files",
+		rules: {
+			"eslint-plugin/require-meta-docs-description": "error",
+			"eslint-plugin/require-meta-docs-url": "error",
+			"eslint-plugin/require-meta-schema": "error",
+		},
+	},
+	{
 		extends: [vitest.configs.recommended],
 		files: ["**/*.test.*"],
+		name: "Test files",
 		rules: { "@typescript-eslint/no-unsafe-assignment": "off" },
 	},
 	{
 		extends: [yml.configs["flat/standard"], yml.configs["flat/prettier"]],
 		files: ["**/*.{yml,yaml}"],
+		name: "YAML files",
 		rules: {
 			"yml/file-extension": "error",
 			"yml/sort-keys": [
@@ -135,6 +144,7 @@ export default defineConfig([
 	},
 	{
 		files: [".github/workflows/**/*.{yml,yaml}"],
+		name: "GitHub Actions workflow files",
 		rules: {
 			"yml/sort-keys": [
 				"error",
@@ -158,10 +168,32 @@ export default defineConfig([
 		},
 	},
 	{
+		extends: [packageJson.configs.recommended, ...configs.json],
+		files: ["package.json", "**/package.json"],
+		name: "Package.json files",
+	},
+	{
+		extends: [...configs.yaml],
+		files: ["pnpm-workspace.yaml"],
+		name: "Pnpm workspace YAML rules",
+	},
+
+	{
 		files: ["*.config.{js,ts}", "**/*.config.{js,ts}"],
+		name: "Configuration files",
 		rules: {
 			"@typescript-eslint/no-unsafe-member-access": "off",
 			"n/no-unsupported-features/node-builtins": "off",
+		},
+	},
+	{
+		extends: [dependabot.configs.recommended],
+		files: ["**/.github/dependabot.{yml,yaml}"],
+		language: "yaml/yaml",
+		name: "Dependabot config",
+		plugins: {
+			dependabot,
+			yaml,
 		},
 	},
 ]);
